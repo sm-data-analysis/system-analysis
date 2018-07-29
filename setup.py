b65a83a5165eb3b464418eb1aabb8648f50dac8d
smazumder05 <smazumder@gmail.com>
2018-07-29 11:12:28 -0400

    initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using data science tools for system analysis',
+    author='smtechnocrat',
+    license='MIT',
+)
